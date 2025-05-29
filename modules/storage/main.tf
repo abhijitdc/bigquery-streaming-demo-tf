e8975d75a9989a9714cf8f222d1f2dd121667988
abhijitdc <abhisaxj@gmail.com>
2025-05-29 08:54:03 -0700

    gcs force destroy

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -10,10 +10,11 @@ resource "random_string" "bucket_suffix" {
 module "bucket" {
   source                   = "github.com/GoogleCloudPlatform/cloud-foundation-fabric/modules/gcs?ref=v36.1.0"
   project_id               = var.project_id
-  prefix                   = var.bucket_prefix 
+  prefix                   = var.bucket_prefix
   name                     = "${var.bucket_prefix}-bucket-${random_string.bucket_suffix.result}" # Constructing name within module
   location                 = var.location
   public_access_prevention = var.public_access_prevention
   versioning               = var.enable_versioning
   labels                   = var.bucket_labels
+  force_destroy            = true
 }
